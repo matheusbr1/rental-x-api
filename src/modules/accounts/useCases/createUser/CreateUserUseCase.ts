600af6a88d3e2c397f3ad454fe2bad4ab0512c44
Matheus Baron <matheusbaron10@gmail.com>
2022-03-16 22:30:56 -0300

    Fix: user email validation added

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { hash } from "bcrypt";
 
 @injectable()
 class CreateUserUseCase {
@@ -10,6 +11,15 @@ class CreateUserUseCase {
   ) {}
 
   async execute({ name, email, driver_license, password }: ICreateUserDTO): Promise<void> {
+
+    const userAlreadyExists = await this.usersRepository.findByEmail(email)
+    
+    if(userAlreadyExists) {
+      throw new Error('User already exists')
+    }
+
+    // const passwordHash = await hash(password, 8)
+
     await this.usersRepository.create({
       name, email, driver_license, password
     })
