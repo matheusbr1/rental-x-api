273faa913173fe2c05087a5b975513cd54c078ac
Matheus Baron <matheusbaron10@gmail.com>
2022-03-22 22:18:37 -0300

    Feat: Errors middleware created

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from "tsyringe"
 import { IUsersRepository } from "../../repositories/IUsersRepository"
 import { compare } from 'bcrypt'
 import { sign } from "jsonwebtoken"
+import { AppError } from "../../../../errors/AppError"
 
 interface IRequest {
   email: string
@@ -27,7 +28,7 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email)
 
     if (!user) {
-      throw new Error('Email or password incorrect')
+      throw new AppError('Email or password incorrect')
     }
 
     // const passwordMatch = await compare(password, user.password)
@@ -35,7 +36,7 @@ class AuthenticateUserUseCase {
     const passwordMatch = password === user.password
 
     if(!passwordMatch) {
-      throw new Error('Email or password incorrect')
+      throw new AppError('Email or password incorrect')
     }
 
     // JWT
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import "express-async-errors"
 import "./database"
 import "./shared/container"
 import { router } from './routes'
 import swaggerUI from 'swagger-ui-express'
 import swaggerFile from './swagger.json'
+import { AppError } from './errors/AppError'
 
 const app = express()
 
@@ -12,6 +14,19 @@ app.use(express.json())
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile))
 
 app.use(router)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if(err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: `Interna server error - ${err.message}`
+  })
+})
 
 const port = 3333
 
