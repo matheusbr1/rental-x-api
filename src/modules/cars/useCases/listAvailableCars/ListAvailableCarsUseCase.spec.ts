2401edfd9b93ec22775c95bb60f7199c49150809
Matheus Baron <matheusbaron10@gmail.com>
2022-04-18 21:56:58 -0300

    Feat: available cars route and controller created

--- src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.spec.ts
@@ -1,13 +1,13 @@
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory"
-import { ListCarsUseCase } from "./ListCarsUseCase"
+import { ListAvailableCarsUseCase } from "./ListAvailableCarsUseCase"
 
-let listCarsUseCase: ListCarsUseCase
+let listAvailableCarsUseCase: ListAvailableCarsUseCase
 let carsRepositoryInMemory: CarsRepositoryInMemory
 
-describe('List Cars', () => {
+describe('List Available Cars', () => {
   beforeEach(() => {
     carsRepositoryInMemory = new CarsRepositoryInMemory()
-    listCarsUseCase = new ListCarsUseCase(carsRepositoryInMemory)
+    listAvailableCarsUseCase = new ListAvailableCarsUseCase(carsRepositoryInMemory)
   })
 
   it('should be able to list all available cars', async () => {
@@ -21,7 +21,7 @@ describe('List Cars', () => {
       category_id: 'category'
      })
 
-    const cars = await listCarsUseCase.execute({})
+    const cars = await listAvailableCarsUseCase.execute({})
 
     expect(cars.length).toBe(1)
     expect(cars).toEqual([car])
@@ -38,7 +38,7 @@ describe('List Cars', () => {
       category_id: 'category'
      })
 
-    const cars = await listCarsUseCase.execute({
+    const cars = await listAvailableCarsUseCase.execute({
       brand: 'Car Brand Test',
     })
 
@@ -56,7 +56,7 @@ describe('List Cars', () => {
       category_id: 'category'
      })
 
-    const cars = await listCarsUseCase.execute({
+    const cars = await listAvailableCarsUseCase.execute({
       name: 'Car Available Test'
     })
 
@@ -74,7 +74,7 @@ describe('List Cars', () => {
       category_id: 'category Test'
      })
 
-    const cars = await listCarsUseCase.execute({
+    const cars = await listAvailableCarsUseCase.execute({
       category_id: 'category Test'
     })
 
