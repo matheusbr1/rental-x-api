6aed01a3bebfb27da32c17eccd2a04f8ef8276f2
Matheus Baron <matheusbaron10@gmail.com>
2022-04-22 10:59:26 -0300

    Feat: rentals migration created

---
+++ src/shared/infra/typeorm/migrations/1650635306080-CreateRentals.ts
@@ -0,0 +1,77 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateRentals1650635306080 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'rentals',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true
+                    },
+                    {
+                        name: 'car_id',
+                        type: 'uuid'
+                    },
+                    {
+                        name: 'user_id',
+                        type: 'uuid'
+                    },
+                    {
+                        name: 'start_date',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'end_date',
+                        type: 'timestamp',
+                    },
+                    {
+                        name: 'expected_return_date',
+                        type: 'timestamp',
+                    },
+                    {
+                        name: 'total',
+                        type: 'numeric'
+                    }, 
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        name: 'FKCarRental', // Nome da chave estrangeira
+                        referencedTableName: 'cars', // Tabela origem
+                        referencedColumnNames: ['id'], // Nome do campo na tabela origem
+                        columnNames: ['car_id'], // Nome do campo na tabela destino
+                        onDelete: 'SET NULL',
+                        onUpdate: 'SET NULL'
+                    },
+                    {
+                        name: 'FKUserRental', // Nome da chave estrangeira
+                        referencedTableName: 'users', // Tabela origem
+                        referencedColumnNames: ['id'], // Nome do campo na tabela origem
+                        columnNames: ['user_id'], // Nome do campo na tabela destino
+                        onDelete: 'SET NULL',
+                        onUpdate: 'SET NULL'
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('rentals')
+    }
+
+}
