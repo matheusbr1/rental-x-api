13ade885d7c127d4353038616691988bc4168b53
Matheus Baron <matheusbaron10@gmail.com>
2022-06-15 23:34:32 -0300

    Test: broken Create Category Controller tests fixed

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -36,7 +36,7 @@ describe('Create Category Controller', () => {
         password: 'admin'
       })
 
-    const { refresh_token } = responseToken.body
+    const { token } = responseToken.body
 
     const response = await request(app)
       .post('/categories')
@@ -44,7 +44,7 @@ describe('Create Category Controller', () => {
         name: 'Category Supertest',
         description: 'Category Supertest'
       }).set({
-        Authorization: 'Bearer ' + refresh_token
+        Authorization: 'Bearer ' + token
       })
 
       expect(response.status).toBe(201)
@@ -57,7 +57,7 @@ describe('Create Category Controller', () => {
         password: 'admin'
       })
 
-      const { refresh_token } = responseToken.body
+      const { token } = responseToken.body
 
     const response = await request(app)
       .post('/categories')
@@ -65,7 +65,7 @@ describe('Create Category Controller', () => {
         name: 'Category Supertest',
         description: 'Category Supertest'
       }).set({
-        Authorization: 'Bearer ' + refresh_token
+        Authorization: 'Bearer ' + token
       })
 
       expect(response.status).toBe(400)
