3275e51d837921705468dd6b260c3c1154ffae10
Matheus Baron <matheusbaron10@gmail.com>
2022-07-13 22:45:54 -0300

    Feat: S3StorageProvider created

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,6 +4,7 @@ import { IMailProvider } from './MailProvider/IMailProvider'
 import { DayJsDateProvider } from './DateProvider/Implementations/DayJsDateProvider'
 import { EtherealMailProvider } from './MailProvider/Implementations/EtherealMailProvider'
 import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider'
+import { S3StorageProvider } from './StorageProvider/implementations/S3StorageProvider'
 
 container.registerSingleton<IDateProvider>(
   'DayJsDateProvider',
@@ -15,7 +16,12 @@ container.registerInstance<IMailProvider>(
   new EtherealMailProvider()
 )
 
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider
+}
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
-  LocalStorageProvider
+  diskStorage[process.env.disk]
 )
